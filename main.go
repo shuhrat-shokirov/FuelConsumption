d9eccf87553c4c7535090fc4633f7f69e74a0fb9
Shuhrat.Shokirov <avatar_9999@list.ru>
2020-01-05 13:39:06 +0300

    Clear main

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	consuption := 8
-	volume := 5
-	fmt.Println(fuels(consuption,volume))
+
 }
 func fuels(consumption int, volume int)int {
 	consumptionPer := volume * 100 / consumption
