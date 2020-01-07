438a49ca6418bdf89e00571175ab8fd2ca09f15c
Shuhrat.Shokirov <avatar_9999@list.ru>
2020-01-07 15:54:44 +0300

    Function fixed

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 func main() {
 
 }
-func fuels(consumption int, volume int)int
+func fuels(consumption int, volume int)int{
 	consumptionper := volume * 100 / consumption
 	return consumptionper
 }
