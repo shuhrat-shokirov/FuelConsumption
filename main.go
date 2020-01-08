df71e3a5015347e2d8fa876de0be673957571f0f
Shuhrat.Shokirov <avatar_9999@list.ru>
2020-01-08 17:16:43 +0300

    Fixed #9

--- main.go
+++ main.go
@@ -4,6 +4,6 @@ func main() {
 
 }
 func fuels(consumption int, volume int)int{
-	consumptionper := volume * 100 / consumption
-	return consumptionper
+	consumptionPer := volume * 100 / consumption
+	return consumptionPer
 }
